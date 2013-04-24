4749173460247bab867b0f65f5f726a636f534a4
Satoshi Ebisawa <e.satoshi@gmail.com>
2013-04-24 20:46:04 +0900

    Display alert

--- HelloWorld/app/app_delegate.rb
+++ HelloWorld/app/app_delegate.rb
@@ -1,5 +1,7 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    alert = UIAlertView.alloc.initWithTitle('Hello, world!', message:'Welcome to RubyMotion!!', delegate:self, cancelButtonTitle:nil, otherButtonTitles:'OK', nil)
+    alert.show
     true
   end
 end
